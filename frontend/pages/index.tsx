4eba7885c57be4392e19fb62ef883f5a13eac423
Elizabeth Foster <elizabeth@rootedfarmers.com>
2022-10-31 00:57:25 +0100

    first commit

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import axios from 'axios';
-import React from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import React from "react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p> Hello, World! </p>
+      <p> Hello, World! Liz and Ruby were here </p>
     </div>
-  )
+  );
 }
