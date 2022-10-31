ff92286bc2e60c1ac9b9b78757855d8231eb6056
emma-mp <emmapaterson135@gmail.com>
2022-10-30 20:02:13 -0400

    bare bones sidebar

---
+++ frontend/components/sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Sidebar() {
+    return (
+        <>
+            <h1>Welcome</h1>
+        </>
+    );
+};
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import axios from 'axios';
 import React from 'react';
+import Sidebar from '../components/Sidebar.js'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <p> Hello, World! </p>
+      <Sidebar></Sidebar>
     </div>
   )
 }
