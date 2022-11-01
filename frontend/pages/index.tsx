cc320f9be2d34435c020d5a39b4ea866ac74e8b9
Elizabeth Foster <elizabeth@rootedfarmers.com>
2022-11-01 00:06:53 +0000

    playing around with grid stuff

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import dashboardStyles from "../styles/Dashboard.module.css";
 import axios from "axios";
 import React from "react";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p> Hello, World! Liz and Ruby were here </p>
+      <div className={dashboardStyles.header1}>
+        <p> Welcome, Ariya </p>
+      </div>
+      <div className={dashboardStyles.grid}>
+        <div className={dashboardStyles.assignmentCard}>Assignment</div>
+        <div className={dashboardStyles.upcomingMeetingCard}>
+          Upcoming Meeting
+        </div>
+        <div className={dashboardStyles.assignmentCard}>Common Questions</div>
+        <div className={dashboardStyles.upcomingMeetingCard}>Events</div>
+        <div className={styles.card}></div>
+      </div>
     </div>
   );
 }
