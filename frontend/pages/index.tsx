fdcc6f88eb4dc92a418bca3539dc1986be70780a
Elizabeth Foster <elizabeth@rootedfarmers.com>
2022-11-03 01:52:44 +0100

    wip

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,13 +12,52 @@ export default function Home() {
         <p> Welcome, Ariya </p>
       </div>
       <div className={dashboardStyles.grid}>
-        <div className={dashboardStyles.assignmentCard}>Assignment</div>
+        <div className={dashboardStyles.assignmentCard}>
+          <div className={dashboardStyles.cardTitle}> Assignments</div>
+          <div className={dashboardStyles.upperRightCornerText}> SEE ALL</div>
+          <div className={dashboardStyles.assignment}>
+            Paired Programming Project Pt. 2
+            <div className={dashboardStyles.dueDate}>
+              Wed, October 7, 9:59 PM
+            </div>
+            <div className={dashboardStyles.arrowIcon}> > </div>
+          </div>
+          <div className={dashboardStyles.assignment}>
+            Week 6 Module of Codecademy
+            <div className={dashboardStyles.dueDate}>
+              Wed, October 7, 9:59 PM
+            </div>
+            <div className={dashboardStyles.arrowIcon}> > </div>
+          </div>
+          <div className={dashboardStyles.assignment}>
+            Week 6 Slides and Recording
+            <div className={dashboardStyles.arrowIcon}> > </div>
+
+          </div>
+          <div className={dashboardStyles.assignment}>All About Github
+            <div className={dashboardStyles.arrowIcon}> > </div>
+
+          </div>
+        </div>
         <div className={dashboardStyles.upcomingMeetingCard}>
           Upcoming Meeting
+          <div className={dashboardStyles.upperRightCornerText}>+ Add More</div>
+          <div className={dashboardStyles.buttonContainer}>
+            <button className={dashboardStyles.joinButton}> Join </button>{" "}
+            <button className={dashboardStyles.rescheduleButton}> Reschedule </button>
+          </div>
         </div>
-        <div className={dashboardStyles.assignmentCard}>Common Questions</div>
-        <div className={dashboardStyles.upcomingMeetingCard}>Events</div>
-        <div className={styles.card}></div>
+        <div className={dashboardStyles.commonQuestionsCard}>
+          Common Questions
+        </div>
+        <div className={dashboardStyles.eventsCard}>
+          Eventsx
+          <div className={dashboardStyles.upperRightCornerText}> SEE ALL</div>
+        </div>
+        <div className={dashboardStyles.yourGoalsCard}>
+          Your Goals
+          <div className={dashboardStyles.upperRightCornerText}> EDIT </div>
+        </div>
       </div>
     </div>
   );
