703caea96b3d08bd9f9e2238371391678313e7d0
Elizabeth Foster <elizabeth@rootedfarmers.com>
2023-02-05 20:11:01 -0500

    wip

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Sidebar from "../components/sidebar";
 import styles from "../styles/Home.module.css";
 
 export default function Login() {
-  return <div className={styles.container}>Login Page</div>;
+  return <div>Login Page</div>;
 }
