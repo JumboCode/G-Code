5a131d8e65f08c47f9ec64a58aa2ea594f5fd09e
maslenj <james.maslen@tufts.edu>
2023-02-20 17:19:42 -0500

    add filter functionality

--- frontend/pages/FAQBoard.tsx
+++ frontend/pages/FAQBoard.tsx
@@ -19,6 +19,8 @@ export default function FAQBoard() {
       numReplies: 4,
       date: "Nov 21",
       time: "3:30 PM",
+      week: 1,
+      topics: ["PHP"],
       pinned: false
     },
     {
@@ -28,6 +30,8 @@ export default function FAQBoard() {
       numReplies: 2,
       date: "Nov 22",
       time: "4:20 PM",
+      week: 1,
+      topics: ["HTML", "CSS"],
       pinned: false
     },
     {
@@ -37,6 +41,8 @@ export default function FAQBoard() {
       numReplies: 1,
       date: "Nov 22",
       time: "4:20 PM",
+      week: 2,
+      topics: ["Administrative"],
       pinned: false
     },
     {
@@ -46,6 +52,8 @@ export default function FAQBoard() {
       numReplies: 0,
       date: "Nov 22",
       time: "4:20 PM",
+      week: 2,
+      topics: ["JavaScript"],
       pinned: false
     },
     {
@@ -55,15 +63,32 @@ export default function FAQBoard() {
       numReplies: 2,
       date: "Nov 22",
       time: "4:20 PM",
+      week: 3,
+      topics: ["Administrative"],
       pinned: false
     },
   ]
 
-  const weeks = ["All Weeks"].concat(Array.from({ length: 10 }, (_, i) => `Week ${i + 1}`))
-  const [week, setWeek] = React.useState<string>("All Weeks");
+  const weeks = ["All Weeks"].concat(Array.from(new Set(questions.map(question => `Week ${question.week}`))))
+  const [week, setWeek] = React.useState<string>(weeks[0]);
 
-  const topics = ["All Topics", "HTML", "CSS", "PHP", "JavaScript"]
+  const topics = ["All Topics"].concat(Array.from(new Set(questions.reduceRight((accumulator, question) => accumulator.concat(question.topics), []))))
   const [topic, setTopic] = React.useState<string>("All Topics");
+
+  const [searchQuery, setSearchQuery] = React.useState<string>("");
+
+  // Filter functions
+  const filterWeek = question => {
+    return (week == "All Weeks") || (week == `Week ${question.week}`)
+  }
+
+  const filterTopic = question => {
+    return (topic == "All Topics") || (question.topics.includes(topic))
+  }
+
+  const filterSearch = question => {
+    return searchQuery == "" || JSON.stringify(question).toUpperCase().includes(searchQuery.toUpperCase())
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -76,7 +101,7 @@ export default function FAQBoard() {
           sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${DRAWER_WIDTH}px)` } }}
         >
           <Grid sx={{ paddingTop: '50px' }} container spacing={2}>
-            <Grid item md={9} sm={12}>
+            <Grid item md={9} xs={12}>
               <Box sx={{ padding: "40px 0 30px 0" }}>
                 <Typography variant="h1">
                   Community Forum
@@ -107,6 +132,8 @@ export default function FAQBoard() {
                         sx={{ ml: 1, flex: 1 }}
                         placeholder="Search"
                         inputProps={{ 'aria-label': 'search' }}
+                        value={searchQuery}
+                        onChange={event => setSearchQuery(event.target.value)}
                       />
                     </Paper>
                   </Grid>
@@ -127,16 +154,16 @@ export default function FAQBoard() {
                 </Grid>
               </Box>
             </Grid>
-            <Grid item md={3} sm={0}>
+            <Grid item md={3} xs={0}>
               {/* Filler */}
             </Grid>
           </Grid>
 
           <Grid container spacing={2}>
-            <Grid item md={9} sm={12}>
+            <Grid item md={9} xs={12}>
               <Card sx={{ borderRadius: '10px' }}>
                 <List sx={{ padding: '0 20px 20px 20px' }}>
-                  {questions.map(question =>
+                  {questions.filter(filterWeek).filter(filterTopic).filter(filterSearch).map(question =>
                     <>
                       <ListItem sx={{ padding: '40px 20px 40px 20px' }}>
                         <ListItemAvatar sx={{ width: '70px' }}>
@@ -160,7 +187,7 @@ export default function FAQBoard() {
                 </List>
               </Card>
             </Grid>
-            <Grid item md={3} sm={12}>
+            <Grid item md={3} xs={12}>
               <Box>
                 <Typography variant="h3">
                   Still Confused?
