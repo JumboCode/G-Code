74321ab71691084dffc34bc2682eebdaf64b5fcd
Elizabeth Foster <elizabeth@rootedfarmers.com>
2023-03-05 19:27:07 -0500

    wip

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -1,47 +1,66 @@
 import axios from "axios";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import dashboardStyles from "../styles/Dashboard.module.css";
 import loginStyles from "../styles/Login.module.css";
-import Sidebar from "../components/sidebar";
-import logo from './public/GCodeLogo.png'
+import logo from "./public/GCodeLogo.png";
+import { theme } from "../theme.ts";
+import { ThemeProvider, TextField, Button, FormControl } from "@mui/material";
 
+export default function Login() {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
 
-// TODO
-// 1. Forgot 
+  // const credentials = {
+  //     username: setUserName,
+  //     password: setPassword
+  // };
 
- 
+  // React.useEffect(() => {
+  //     axios.put('http://localhost:8000/api/login/', credentials)
+  // }, []);
 
-    export default function Login() {
-        const [username, setUserName] = useState();
-        const [password, setPassword] = useState();
-
-        // const credentials = {
-        //     username: setUserName,
-        //     password: setPassword
-        // };
-
-        // React.useEffect(() => {
-        //     axios.put('http://localhost:8000/api/login/', credentials)
-        // }, []);
-
-        return (
-            <div className={loginStyles.container}>
-                <div className={loginStyles.formContainer}>
-                <h1 className={loginStyles.signInText}> Sign In </h1>
-                <form action="" method="get" id="signIn" className={loginStyles.form}>
-                    <input type="text" name="username" placeholder="Email Address" onChange={(e) => setUserName(e.target.value)}/>
-                    <input type="password" id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className={loginStyles.password}/>
-                    <p className={loginStyles.forgotPassword}> Forgot Password? </p>
-                    <div className={loginStyles.loginButton} onClick={() => console.log(username, password)}>
-                    Sign In
-                </div>
-                </form>
-                </div>
-            </div>
-        )
-        
-    }
-    
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={loginStyles.container}>
+        <div className={loginStyles.box}>
+          <div className={loginStyles.formContainer}>
+            <h1 className={loginStyles.signInText}> Sign In </h1>
+            <FormControl fullWidth>
+              <form
+                action=""
+                method="get"
+                id="signIn"
+                className={loginStyles.form}
+              >
+                <TextField
+                  // sx={(margin: "10pc")}
+                  id="outlined-basic"
+                  label="Email Address"
+                  variant="outlined"
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Password"
+                  variant="outlined"
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+                <p className={loginStyles.forgotPassword}> Forgot Password? </p>
+                <Button
+                  variant="primary"
+                  className={loginStyles.loginButton}
+                  onClick={() => console.log(username, password)}
+                >
+                  Sign In
+                </Button>
+              </form>
+            </FormControl>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
