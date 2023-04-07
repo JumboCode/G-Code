89575b90c58452d400d4dd24667fade94d4944ff
Elizabeth Foster <elizabeth@rootedfarmers.com>
2023-04-07 08:49:30 -0400

    wip

--- frontend/components/authentification.tsx
+++ frontend/components/authentification.tsx
@@ -1,38 +1,36 @@
-import axios from 'axios';
-import { useEffect, Component, ComponentType, FC } from 'react';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import { useEffect, Component, ComponentType, FC } from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 
 export default function IsUserAuthorized(required_permission, save_user) {
   const router = useRouter();
-  const permissions = { "None": 0, "Student": 1, "Admin": 2 }
+  const permissions = { None: 0, Student: 1, Admin: 2 };
   useEffect(() => {
     const validateSession = async () => {
-      const token = Cookies.get('gcode-session');
+      const token = Cookies.get("gcode-session");
       try {
         const config = {
           headers: {
-            'accept': 'application/json',
-            'Authorization': 'Bearer ' + token
-          }
+            accept: "application/json",
+            Authorization: "Bearer " + token,
+          },
         };
-        
-        let result = await axios.get('http://localhost:8000/', config)     
-        
-        console.log(result)
 
-        let user_permission = result.data["permission_level"]
-        save_user(result.data)
+        let result = await axios.get("http://localhost:8000/", config);
+
+        console.log(result);
+
+        let user_permission = result.data["permission_level"];
+        save_user(result.data);
         if (permissions[user_permission] < permissions[required_permission]) {
-          throw new Error('Invalid Permission');
+          throw new Error("Invalid Permission");
         }
       } catch (error) {
-        console.log(error)
-        // router.push('/Login');
->>>>>>> main
+        console.log(error);
+        router.push("/Login");
       }
     };
     validateSession();
   });
 }
-
