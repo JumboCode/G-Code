1b6e621c8d45e52e19ae9102a74d2b5bb6224e57
Elizabeth Foster <elizabeth@rootedfarmers.com>
2023-04-19 15:53:08 -0400

    ignore this

--- frontend/components/admin_pages/AdminDashboard.tsx
+++ frontend/components/admin_pages/AdminDashboard.tsx
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Box, CssBaseline, Button } from "@mui/material";
 import TutoringCardDisplay from "../tutoringCard";
-import styles from '../../styles/Home.module.css'
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import styles from "../../styles/Home.module.css";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import CommunityResourcesPanel from "../communityResourcesPanel";
 import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function Dashboard(props) {
-  const user = props.user
-  const router = useRouter()
+  const user = props.user;
+  const router = useRouter();
 
   // make call to backend to get real data
   const [assignmentList, setAssignmentList] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/assignments', {
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-      .then(response => {
-        setAssignmentList(response.data)
+    axios
+      .get("http://localhost:8000/api/assignments", {
+        headers: {
+          Accept: "application/json",
+        },
       })
-      .catch(error => {
+      .then((response) => {
+        setAssignmentList(response.data);
+      })
+      .catch((error) => {
         console.log(error);
       });
   }, []);
@@ -47,26 +48,35 @@ export default function Dashboard(props) {
             <TutoringCardDisplay />
           </div>
 
-          {assignmentList.length > 0 &&
+          {assignmentList.length > 0 && (
             <>
-              <div className={styles.header2}>{assignmentList.length} Assignment{assignmentList.length > 1 && "s"}</div>
+              <div className={styles.header2}>
+                {assignmentList.length} Assignment
+                {assignmentList.length > 1 && "s"}
+              </div>
 
-
-              <List className={styles.pageElement} sx={{ backgroundColor: 'white' }}>
-                {assignmentList.map(assignment => (
+              <List
+                className={styles.pageElement}
+                sx={{ backgroundColor: "white" }}
+              >
+                {assignmentList.map((assignment) => (
                   <ListItem
                     key={assignment.name}
                     secondaryAction={
-                      <IconButton edge="end" aria-label="delete" onClick={() => {
-                        router.push('/Assignments')
-                      }}>
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => {
+                          router.push("/Assignments");
+                        }}
+                      >
                         <ArrowForwardIosIcon />
                       </IconButton>
                     }
                   >
                     <ListItemAvatar>
                       <Avatar sx={{ backgroundColor: "#F5F7F9" }}>
-                        <img src="./AssignmentIcon.svg" />
+                        {/* <img src="./AssignmentIcon.svg" /> */}
                       </Avatar>
                     </ListItemAvatar>
                     <ListItemText
@@ -77,8 +87,7 @@ export default function Dashboard(props) {
                 ))}
               </List>
             </>
-          }
-
+          )}
         </Grid>
         <Grid item xs={12} md={4}>
           <div className={styles.header2}> Schedule OH </div>
@@ -86,18 +95,26 @@ export default function Dashboard(props) {
           <Grid container spacing={2}>
             <Grid item xs={6} md={12} lg={6}>
               <div className={styles.question}>
-                <img src="FAQBoardIcon.svg" onClick={() => {
-                  router.push('/OfficeHours')
-                }} /><br />
+                {/* <img
+                  src="FAQBoardIcon.svg"
+                  onClick={() => {
+                    router.push("/OfficeHours");
+                  }}
+                /> */}
+                <br />
                 FAQ Board
                 {/* NEED TO ADD PROPER LINK: GOES TO FAQBOARD */}
               </div>
             </Grid>
             <Grid item xs={6} md={12} lg={6}>
               <div className={styles.question}>
-                <img src="FAQBoardIcon.svg" onClick={() => {
-                  router.push('/FAQBoard')
-                }} /><br />
+                {/* <img
+                  src="FAQBoardIcon.svg"
+                  onClick={() => {
+                    router.push("/FAQBoard");
+                  }}
+                /> */}
+                <br />
                 FAQ Board
               </div>
             </Grid>
