47d8390eca4962761332c99dd5333d272aff5d05
maslenj <james.maslen@tufts.edu>
2023-04-24 11:44:02 -0400

    add gradient and responsiveness to login + registration

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -14,75 +14,84 @@ import Cookies from 'js-cookie';
 import Link from "next/link";
 
 export default function Login() {
-    const router = useRouter();
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [loginError, setloginError] = useState(false);
+  const router = useRouter();
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setloginError] = useState(false);
 
 
-    const postInfo = () => {
-        const postData = {
-            grant_type: '',
-            username: username,
-            password: password,
-            scope: '',
-            client_id: '',
-            client_secret: ''
-        };
+  const postInfo = () => {
+    const postData = {
+      grant_type: '',
+      username: username,
+      password: password,
+      scope: '',
+      client_id: '',
+      client_secret: ''
+    };
 
-        axios.post('http://localhost:8000/login', postData, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'accept': 'application/json'
-            }
-        })
-            .then(function success(response) {
-                console.log(response)
-                const token = response.data.access_token;
-                Cookies.set('gcode-session', token, { expires: 7 });
-                router.push('/Dashboard');
-            })
-            .catch(function failure(error) {
-                console.log(error);
-                setloginError(true);
-            })
-    }
-
-    return (
-      <ThemeProvider theme={theme}>
-        <div className={loginStyles.container}>
-          <Box component="form" sx={{ width: "30pc", alignItems: "center" }}>
-            <h1 className={loginStyles.signInText}> Sign In </h1>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="Email Address"
-              variant="outlined"
-              sx={{ margin: "10px" }}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                setUserName(event.target.value);
-              }}
-            />
-            <TextField
-              fullWidth
-              id="outlined-helperText"
-              label="Password"
-              type="password"
-              sx={{ margin: "10px" }}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                setPassword(event.target.value);
-              }}
-            />
-            <text className={loginStyles.forgotPassword}>Forgot Password?</text>
-            <Button variant="primary" sx={{ margin: "10px", width: "30pc" }} onClick={postInfo}>
-              <h1 className={loginStyles.signInButton}>Login </h1>
-            </Button>
-          </Box>
-          <text className={loginStyles.haveAccount}>
-            New to GCode?
-            <Link href="./Registration"> <text className={loginStyles.signIn}> Create an Account.</text> </Link>
-          </text>
-        </div>
-      </ThemeProvider>
-    );
+    axios.post('http://localhost:8000/login', postData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'accept': 'application/json'
+      }
+    })
+      .then(function success(response) {
+        console.log(response)
+        const token = response.data.access_token;
+        Cookies.set('gcode-session', token, { expires: 7 });
+        router.push('/Dashboard');
+      })
+      .catch(function failure(error) {
+        console.log(error);
+        setloginError(true);
+      })
   }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={loginStyles.container}>
+        <div
+          style={{
+            padding: '80px',
+            background: '#fefefe',
+            boxShadow: '0px 4px 24px rgba(0, 0, 0, 0.25)',
+            borderRadius: '12px',
+          }}
+        >
+        <Box component="form" sx={{ width: "30pc", alignItems: "center" }}>
+          <h1 className={loginStyles.signInText}> Sign In </h1>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Email Address"
+            variant="outlined"
+            sx={{ marginBottom: "20px" }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setUserName(event.target.value);
+            }}
+          />
+          <TextField
+            fullWidth
+            id="outlined-helperText"
+            label="Password"
+            type="password"
+            sx={{ marginBottom: "20px" }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <text className={loginStyles.forgotPassword}>Forgot Password?</text>
+          <Button fullWidth variant="primary" sx={{margin: '20px 0 10px 0'}} onClick={postInfo}>
+            <h1 className={loginStyles.signInButton}>Login </h1>
+          </Button>
+        </Box>
+        <text className={loginStyles.haveAccount}>
+          New to GCode?
+          <Link href="./Registration"> <text className={loginStyles.signIn}> Create an Account.</text> </Link>
+        </text>
+      </div>
+    </div>
+    </ThemeProvider >
+  );
+}
--- frontend/pages/Registration.tsx
+++ frontend/pages/Registration.tsx
@@ -10,6 +10,7 @@ import logo from "./public/GCodeLogo.png";
 import { Button, ThemeProvider, TextField, Box } from "@mui/material";
 import { theme } from "../theme.ts";
 import { useRouter } from "next/router";
+import Grid from '@mui/material/Grid'
 import Cookies from "js-cookie";
 import Link from "next/link";
 
@@ -25,86 +26,96 @@ export default function Registration() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [code, setCode] = useState();
-  const [zoom, setZoom] = useState();   
+  const [zoom, setZoom] = useState();
   const [regError, setRegError] = useState(false);
 
   /* POST DATA */
   const postRegInfo = () => {
-        const postRegData = {
-            firstname: firstName,
-            lastname: lastName,
-            email: email,
-            password: password,
-            code: code,
-            zoom: zoom
-        };
+    const postRegData = {
+      firstname: firstName,
+      lastname: lastName,
+      email: email,
+      password: password,
+      code: code,
+      zoom: zoom
+    };
 
-        axios.put('http://localhost:8000/registration', postRegData, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'accept': 'application/json'
-            }
-        })
-            .then(function success(response) {
-                console.log(response)
-                const token = response.data.access_token;
-                Cookies.set('gcode-session', token, { expires: 7 });
-                router.push('/Login');
-            })
-            .catch(function failure(error) {
-                console.log(error);
-                setRegError(true);
-            })
-    }
+    axios.put('http://localhost:8000/registration', postRegData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'accept': 'application/json'
+      }
+    })
+      .then(function success(response) {
+        console.log(response)
+        const token = response.data.access_token;
+        Cookies.set('gcode-session', token, { expires: 7 });
+        router.push('/Login');
+      })
+      .catch(function failure(error) {
+        console.log(error);
+        setRegError(true);
+      })
+  }
   /* END POST DATA */
 
 
   return (
     <ThemeProvider theme={theme}>
       <div className={loginStyles.container}>
-        <Box component="form" sx={{ width: "30pc", alignItems: "center" }}>
+        <Box component="form" sx={{
+          width: "40pc",
+          alignItems: "center",
+          backgroundColor: '#fefefe',
+          borderRadius: '12px',
+          padding: '40px 80px',
+        }}
+
+        >
           <h1 className={loginStyles.signInText}> Create an Account </h1>
+          <Grid container spacing={2}>
+            <Grid item xs="6">
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="First name"
+                variant="outlined"
+                sx={{ margin: "10px" }}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setFirstName(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs="6">
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                sx={{ margin: "10px" }}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setEmail(event.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+
           <TextField
-            id="outlined-basic"
-            label="First name"
-            variant="outlined"
-            sx={{ margin: "10px", width: "13.75pc" }}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setFirstName(event.target.value);
-            }}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Last name"
-            variant="outlined"
-            sx={{ margin: "10px", width: "13.75pc" }}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setLastName(event.target.value);
-            }}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            sx={{ margin: "10px", width: "28.8pc" }}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setEmail(event.target.value);
-            }}
-          />
-          <TextField
+            fullWidth
             id="outlined-helperText"
             label="Password"
             type="password"
-            sx={{ margin: "10px", width: "28.8pc" }}
+            sx={{ margin: "10px" }}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               setPassword(event.target.value);
             }}
           />
           <TextField
+            fullWidth
             id="outlined-basic"
             label="Class Code"
             variant="outlined"
-            sx={{ margin: "10px", width: "28.8pc" }}
+            sx={{ margin: "10px" }}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               setCode(event.target.value);
             }}
@@ -112,23 +123,24 @@ export default function Registration() {
 
           {/* TODO: Make only for admins */}
           <TextField
+            fullWidth
             id="outlined-basic"
             label="Zoom Link"
             variant="outlined"
-            sx={{ margin: "10px", width: "28.8pc" }}
+            sx={{ margin: "10px" }}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               setZoom(event.target.value);
             }}
           />
 
-          <Button variant="primary" sx={{ margin: "10px", width: "28.8pc" }} onClick={postRegInfo}>
+          <Button fullWidth variant="primary" sx={{ margin: "10px" }} onClick={postRegInfo}>
             <h1 className={loginStyles.signInButton}>Create Account</h1>
           </Button>
+          <text className={loginStyles.haveAccount}>
+            Already have an account?
+            <Link href="./Login"> <text className={loginStyles.signIn}> Sign in.</text> </Link>
+          </text>
         </Box>
-        <text className={loginStyles.haveAccount}>
-          Already have an account?
-          <Link href="./Login"> <text className={loginStyles.signIn}> Sign in.</text> </Link>
-        </text>
       </div>
     </ThemeProvider>
   );
